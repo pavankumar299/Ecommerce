6623d2c7edd53b796fd6c999c4237b9ef318019f
Pavan Kumar <pavankumar299@example.com>
2026-04-17 16:28:40 +0530

    frontend change

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const API = axios.create({
-    baseURL:"http://localhost:5000/api"
+    baseURL:import.meta.env.VITE_API_URL
 })
 API.interceptors.request.use((req)=>{
     const token=localStorage.getItem("token")
